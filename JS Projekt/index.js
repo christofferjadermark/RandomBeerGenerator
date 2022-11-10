1827b2c8464098cd89670415e7b5e4db06e60807
christofferjadermark <104230983+christofferjadermark@users.noreply.github.com>
2022-11-10 19:32:11 +0100

    initial commit

--- JS Projekt/index.js
+++
@@ -1,8 +0,0 @@
-const url = "https://api.punkapi.com/v2/beers?page=2&per_page=80";
-
-async function apiCall(url) {
-  const response = await fetch(url);
-  const json = await response.json();
-  console.log(json);
-}
-apiCall(url);
---
+++ index.js
@@ -0,0 +1,49 @@
+const beerContainer = document.querySelector(".beer-container");
+const button = document.querySelector(".generate_button");
+const url = "https://api.punkapi.com/v2/beers/random";
+
+async function apiCall(url) {
+  const response = await fetch(url);
+  const json = await response.json();
+  console.log(json[0]);
+
+  generateHtml(json);
+}
+apiCall(url);
+
+button.addEventListener("click", () => {
+  apiCall(url);
+});
+
+function generateHtml(beerArray) {
+  if (beerArray[0].image_url) {
+    beerContainer.innerHTML = `
+    <div class ="align-items-center row" >
+    <h3 >${beerArray[0].name}</h3>
+    <p>${beerArray[0].tagline}</p>
+    <div class="img_container">
+    <img class="beer_img" src="${beerArray[0].image_url}" alt="">
+    </div>
+    <p>${beerArray[0].description}</p>
+    <ul class="list-group">
+    <li class="list-group-item">${beerArray[0].ph}</li>
+    <li class="list-group-item">${beerArray[0].volume.value} ${beerArray[0].volume.unit}</li>
+    <li class="list-group-item">${beerArray[0].first_brewed}</li>
+    </ul>
+    `;
+  } else {
+    beerContainer.innerHTML = `
+    <h3>${beerArray[0].name}</h3>
+    <p>${beerArray[0].tagline}</p>
+    <img src="https://static.specsonline.com/wp-content/themes/Specs%20Theme/images/default_beer.png" alt="" />
+    <p>${beerArray[0].description}</p>
+    <ul class="list-group">
+    <li class="list-group-item">${beerArray[0].ph}</li>
+    <li class="list-group-item">${beerArray[0].volume.value} ${beerArray[0].volume.unit}</li>
+    <li class="list-group-item">${beerArray[0].first_brewed}</li>
+    </ul>
+    </div>
+
+    `;
+  }
+}
